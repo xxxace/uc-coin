d1a89dfef2f5460af37a3d2844ff6cfcb632bd6f
xxxace <731162160@qq.com>
2023-04-06 21:33:51 +0800

    code: optimized code

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -2,22 +2,22 @@ import { useEffect, useState } from 'react'
 import { Switch } from '@headlessui/react'
 
 export default function MySwitch({ checked, icon, ...props }) {
-    const [enabled, setEnabled] = useState(checked || false)
+    // const [enabled, setEnabled] = useState(checked || false)
 
-    useEffect(() => {
-        setEnabled(checked)
-    }, [checked])
+    // useEffect(() => {
+    //     setEnabled(checked)
+    // }, [checked])
 
     return (
         <Switch
-            checked={enabled}
+            checked={checked}
             {...props}
-            className={`${enabled ? 'bg-zinc-800 text-[#0ea5e9]' : 'bg-gray-200'
+            className={`${checked ? 'bg-zinc-800 text-[#0ea5e9]' : 'bg-gray-200'
                 } relative inline-flex h-6 w-11 items-center rounded-full`}
         >
             <span className="sr-only">Enable notifications</span>
             <span
-                className={`${enabled ? 'translate-x-6 bg-black' : 'translate-x-1 bg-white'
+                className={`${checked ? 'translate-x-6 bg-black' : 'translate-x-1 bg-white'
                     } inline-block h-4 w-4 transform rounded-full transition`}
             >{icon ? icon : ''}</span>
         </Switch>
