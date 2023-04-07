4f7c49422f9b8a83881370646e746015b3d210f8
xxxace <731162160@qq.com>
2023-04-07 11:08:19 +0800

    refactor: refactor the ui and logic

--- components/Contracts/SetPledgeCfg.tsx
+++ components/Contracts/SetPledgeCfg.tsx
@@ -144,60 +144,171 @@ export function setPledgeCfg({ pledgeToken, payToken, profitToken, pledgeReturn,
 }
 
 export default function SetPledgeCfg() {
-    const [tokenAddr, setTokenAddr] = useState('')
+    const [pledgeToken, setPledgeToken] = useState<PledgeToken>({
+        token: `0x` as `0x${string}`,
+        tokenId: BigNumber.from(0),
+        ercType: 0,
+        amount: BigNumber.from(0)
+    })
+    const [payToken, setPayToken] = useState<PayToken>({
+        token: `0x` as `0x${string}`,
+        tokenId: BigNumber.from(0),
+        ercType: 0,
+        amount: BigNumber.from(0)
+    })
+    const [profitToken, setProfitToken] = useState<ProfitToken>({
+        token: `0x` as `0x${string}`,
+        tokenId: BigNumber.from(0),
+        ercType: 0,
+        amount: BigNumber.from(0)
+    })
+    const [pledgeReturn, setPledgeReturn] = useState<PledgeReturn>({
+        profitType: 0,
+        isDayRelease: false,
+        profit: BigNumber.from(0)
+    })
     const [pledgeDays, setPledgeDays] = useState(0)
-    const [data, setData] = useState<SendTransactionResult | undefined>()
-    const [isLoading, setIsLoading] = useState(false)
-    const [isSuccess, setIsSuccess] = useState(false)
-    const [write, setWrite] = useState<(() => void) | undefined>()
 
-    const { data: pledgeCfg, isLoading: pledgeCfgIsLoading, isError: pledgeCfgIsError } = getPledgeCfg(tokenAddr as `0x${string}`, pledgeDays)
+    const { data, isLoading, isSuccess, isError, write, error } = setPledgeCfg({ pledgeToken, payToken, profitToken, pledgeReturn, pledgeDays })
 
-    if (pledgeCfg) {
-        const res = setPledgeCfg(pledgeCfg)
-        setData(res.data)
-        setIsLoading(res.isLoading)
-        setIsSuccess(res.isSuccess)
-        setWrite(res.write)
-    }
+    const setPledgeTokenByKey = (key, e) => {
+        const value = e.target.value;
+        setPledgeToken(prevState => ({
+            ...prevState,
+            [key]: value
+        }));
+    };
 
+    const setPayTokenByKey = (key, e) => {
+        const value = e.target.value;
+        setPayToken(prevState => ({
+            ...prevState,
+            [key]: value
+        }));
+    };
+
+    const setProfitTokenByKey = (key, e) => {
+        const value = e.target.value;
+        setProfitToken(prevState => ({
+            ...prevState,
+            [key]: value
+        }));
+    };
+
+    const setPledgeReturnByKey = (key, e) => {
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setPledgeReturn(prevState => ({
+            ...prevState,
+            [key]: value
+        }));
+    };
 
     return (
         <div className={styles.borderedDiv} >
             <p>
                 <span className={styles.boldText}>配置质押Token(721), 支付token，收益token，收益类型，质押天数</span>。
             </p>
-            <label>
-                Token Address:
-                <input type="text" value={tokenAddr} onChange={e => setTokenAddr(e.target.value)} />
-            </label>
-            <label>
-                Pledge Days:
-                <input type="number" value={pledgeDays} onChange={e => setPledgeDays(Number(e.target.value))} />
-            </label>
-
-            {<div>
-                {pledgeCfgIsLoading ? (
-                    <p>加载PledgeCfg中...</p>
-                ) : pledgeCfgIsError ? (
-                    <p>查询PledgeCfg出错，请重试。</p>
-                ) : (
-                    <>
-                        <p>getPledgeCfg返回值：{JSON.stringify(pledgeCfg)}</p>
-                        <button disabled={!write || isLoading} onClick={() => write && write()}>
-                            {isLoading ? 'Writing...' : 'SetPledgeCfg'}
-                        </button>
-                        {isSuccess && (
+            <div>
+                <h2 className="font-bold">Pledge Token：</h2>
+                <label>
+                    Token:
+                    <input type="text" value={pledgeToken.token} onChange={e => setPledgeTokenByKey('token', e)} />
+                </label>
+                <label>
+                    TokenId:
+                    <input type="text" value={pledgeToken.tokenId as unknown as string} onChange={e => setPledgeToken({ ...pledgeToken, tokenId: BigNumber.from(e.target.value) })} />
+                </label>
+                <label>
+                    ErcType:
+                    <input type="text" value={pledgeToken.ercType} onChange={e => setPledgeTokenByKey('ercType', e)} />
+                </label>
+                <label>
+                    Amount:
+                    <input type="text" value={pledgeToken.amount as unknown as string} onChange={e => setPledgeToken({ ...pledgeToken, amount: BigNumber.from(e.target.value) })} />
+                </label>
+            </div>
+            <div>
+                <h2 className="font-bold">Pay Token：</h2>
+                <label>
+                    Token:
+                    <input type="text" value={payToken.token} onChange={e => setPayTokenByKey('token', e)} />
+                </label>
+                <label>
+                    TokenId:
+                    <input type="text" value={payToken.tokenId as unknown as string} onChange={e => setPayToken({ ...payToken, tokenId: BigNumber.from(e.target.value) })} />
+                </label>
+                <label>
+                    ErcType:
+                    <input type="text" value={payToken.ercType} onChange={e => setPayTokenByKey('ercType', e)} />
+                </label>
+                <label>
+                    Amount:
+                    <input type="text" value={payToken.amount as unknown as string} onChange={e => setPayToken({ ...payToken, amount: BigNumber.from(e.target.value) })} />
+                </label>
+            </div>
+            <div>
+                <h2 className="font-bold">Profit Token：</h2>
+                <label>
+                    Token:
+                    <input type="text" value={profitToken.token} onChange={e => setProfitTokenByKey('token', e)} />
+                </label>
+                <label>
+                    TokenId:
+                    <input type="text" value={profitToken.tokenId as unknown as string} onChange={e => setProfitToken({ ...profitToken, tokenId: BigNumber.from(e.target.value) })} />
+                </label>
+                <label>
+                    ErcType:
+                    <input type="text" value={profitToken.ercType} onChange={e => setProfitTokenByKey('ercType', e)} />
+                </label>
+                <label>
+                    Amount:
+                    <input type="text" value={profitToken.amount as unknown as string} onChange={e => setProfitToken({ ...profitToken, amount: BigNumber.from(e.target.value) })} />
+                </label>
+            </div>
+            <div>
+                <h2 className="font-bold">Pledge Return：</h2>
+                <label>
+                    ProfitType:
+                    <input type="text" value={pledgeReturn.profitType} onChange={e => setPledgeReturnByKey('profitType', e)} />
+                </label>
+                <label>
+                    IsDayRelease:
+                    <input type="checkbox" checked={pledgeReturn.isDayRelease} onChange={e => setPledgeReturnByKey('isDayRelease', e)} />
+                </label>
+                <label>
+                    Profit:
+                    <input type="text" value={pledgeReturn.profit as unknown as string} onChange={e => setPledgeReturn({ ...pledgeReturn, profit: BigNumber.from(e.target.value) })} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <span className="font-bold">Pledge Days:</span>
+                    <input type="number" value={pledgeDays} onChange={e => setPledgeDays(Number(e.target.value))} />
+                </label>
+            </div>
+            {
+                <>
+                    {/* <p>getPledgeCfg返回值：{JSON.stringify(pledgeCfg)}</p> */}
+                    <button className="disabled:opacity-75 disabled:cursor-not-allowed" disabled={!write || isLoading} onClick={() => write && write()}>
+                        {isLoading ? 'Writing...' : 'SetPledgeCfg'}
+                    </button>
+                    {isSuccess && (
+                        <div>
+                            Successfully SetPledgeCfg!
                             <div>
-                                Successfully SetPledgeCfg!
-                                <div>
-                                    <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
-                                </div>
+                                <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
                             </div>
-                        )}
-                    </>
-                )}
-            </div>}
+                        </div>
+                    )}
+                    {
+                        isError && (
+                            <div>
+                                {JSON.stringify(error)}
+                            </div>
+                        )
+                    }
+                </>
+            }
         </div>
     )
 }
