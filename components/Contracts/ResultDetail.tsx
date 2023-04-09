71e604638a7a61239d4fa69f858b3471135cc49a
xxxace <731162160@qq.com>
2023-04-09 17:16:56 +0800

    fix: bigNumber value toNumber overflow change it to toString

--- components/Contracts/ResultDetail.tsx
+++ components/Contracts/ResultDetail.tsx
@@ -10,7 +10,7 @@ export default function ResultDetail(props: { data: PToken | undefined }) {
                         <p>token: {data.token}</p>
                         <p>tokenId: {data.tokenId.toHexString()}</p>
                         <p>地址类型(ercType): {data.ercType}</p>
-                        <p>数量(amount): {data.amount.toNumber()}</p>
+                        <p>数量(amount): {data.amount.toString()}</p>
                     </div>
                 )
                 : '暂无数据'
